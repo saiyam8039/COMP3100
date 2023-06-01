608c4d01b1738e71c6147a9312dc42f11facc197
saiyam8039 <60720320+saiyam8039@users.noreply.github.com>
2023-06-01 15:47:39 +1000

    Add files via upload
    assignment 2 stage 2

--- Client.java
+++ Client.java
@@ -4,6 +4,14 @@ import java.net.*;
 
 public class Client {
 
+	public static String reply;
+	public static int jobID;
+	public static int core;
+	public static int memory;
+	public static int diskSpace;
+	public static List<String[]> getServers = new ArrayList<String[]>();
+
+	
     public static void main(String[] args) {
         try{
             Socket s = new Socket("localhost", 50000);
@@ -36,11 +44,35 @@ public class Client {
             String[] jobInfo = str.split(" ");
             jobID = Integer.parseInt(jobInfo[2]);
             
-            sendMsg("K");
+            sendMsg("OK");
             for (int i = 0; i< serverNumber; i++){
             	String[] serverInfo = str.split(" ");
-            	if(Integer.parseInt(serverInfo[4] > serverCore){
+            	if(Integer.parseInt(serverInfo[4] > serverCore )) {
+                    typeNumber = 1;
+                    serverType = serverInfo[0];
+                    serverID = Integer.parseInt(serverInfo[1]);
+                    serverCore = Integer.parseInt(serverInfo[4]);
+                }
+            }
+            LRRflag = false;
+            sendMsg("OK");
 
+	while (!reply.equals("NONE")) {
+			if (reply.equals("JOBN")){
+			
+                out.write(("Gets Available " + core + " " + memory + " " + diskSpace + "\n").getBytes());
+				out.flush();
+				findData(in.readLine());
+
+				out.write(("OK\n").getBytes());
+				out.flush();
+					
+				in.readLine();
+					
+
+			}
+			
+		}
 	
 
             out.write(("QUIT\n").getBytes());
